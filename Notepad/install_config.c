a18b6816aca408c9bfc64a26f3c8a0b6f0c9ee57
Xipan Zhu <xpzhu@chiponeic.com>
2017-11-08 14:42:11 +0800

    [add]notepd++

---
+++ Notepad/install_config.c
@@ -0,0 +1,7 @@
+/***notepad++ 查找命令***/
+1> 正则表达式“或”搜索 ： 关键词_1|关键词_2, 注“|” 前后没有空行，这样就能按“或”搜索关键词了
+2> 正则表达式“长字符串”搜索： 关键词_1.*?关键词_2
+
+/***notepad++ 环境配置***/
+1> 设置-> 语言格式设置-> 主题 选择“Monokai”
+2> 设置"Default style" and "current line background" and "selected text color" and "line number margin"
