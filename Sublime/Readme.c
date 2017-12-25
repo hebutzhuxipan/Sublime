31a0c0b56a9233290bf990a2f8054d8f06a935f3
xpzhu <xpzhu@chiponeic.com>
2017-12-25 18:56:01 +0800

    [add]文件操作

--- Sublime/Readme.c
+++ Sublime/Readme.c
@@ -1,9 +1,15 @@
 Sublime + git
 1> ctrl+ shift+ p GIT: 执行操作
 
-Sbulime + find
+Sublime + find
 1> ctrl+f (find)
 2> enter (find_next)
 3> shift+enter(find_prev)
 4> alt+enter(find_all)
 5> ctrl+l(expand_selection)
+
+Sublime + file
+1>ctrl+n(new_file)
+2>ctrl+f4(close_file)
+3>ctrl+w(close)//点“X”
+
